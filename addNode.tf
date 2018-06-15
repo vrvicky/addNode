5cee7992bb627f6ca77df320a0b3e2236d670da4
Vignesh <vrvignesh@in.ibm.com>
2018-06-15 21:58:19 +0530

    commit

---
+++ addNode.tf
@@ -0,0 +1,17 @@
+### to add node###
+resource "null_resource" "addNode" {
+  provisioner "remote-exec" {
+      inline = [
+        "docker login -u token -p '${var.reg_token}' ${var.dockerRegistry}",
+        "cd ${var.clusterLocation}",
+        "docker run -e LICENSE=accept --net=host -v $(pwd):/installer/cluster ${var.dockerRegistry}/mdelder/icp-inception:${var.image_tag} ${var.nodeType} -l ${join(",", var.NewNodeIPs)}",
+        "echo ${var.dependsOn}",
+      ]
+  connection {
+    host = "${var.bootIP}"
+    type     = "ssh"
+    user     = "root"
+    password = "${var.root_password}"
+  }
+ }
+}
---
+++ variable.tf
@@ -0,0 +1,36 @@
+variable "bootIP" {
+  description = "IP of host to ssh, boot node or Master 1"
+}
+
+variable "nodeType" {
+  description = "Node type - worker , master, proxy..."
+}
+
+variable "NewNodeIPs" {
+  description = "IP of new node"
+  type = "list"
+}
+
+variable "root_password" {
+  description = "ssh root password"
+}
+
+variable "dockerRegistry" {
+ description = "Docker registry to pull the images"
+ default = "registry.ng.bluemix.net"
+}
+
+variable "reg_token" {
+ description = "Docker registry token for auth"
+ default = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmZTI1ZDQzZS1iNzBlLTU2NDctOGE5Yy0wNjFjZjUyZDI2ODYiLCJpc3MiOiJyZWdpc3RyeS5ibHVlbWl4Lm5ldCJ9.Pn9Vf7sNCQahDWfj2Xi9kGXzq-0hGFnAGhqqpUrWwt4"
+}
+
+variable "image_tag" {
+  description = "GA version of CAM x86"
+}
+
+variable "clusterLocation" {
+  description = "Location to CAM cluster folder"
+}
+
+variable "dependsOn" {}
